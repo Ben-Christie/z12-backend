a2cc8f8a3d7ff0469461b2132655bb043095156c
Ben-Christie <ben.christie11@gmail.com>
2023-02-07 01:20:09 +0000

    removed static files

--- z12_server/settings.py
+++ z12_server/settings.py
@@ -61,7 +61,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
     'login_register_app',
