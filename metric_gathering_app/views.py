1d1c1f515ae8da2f03f8cb1adef709bc2d4df03c
Ben-Christie <ben.christie11@gmail.com>
2023-03-26 15:27:49 +0100

    minor change

--- metric_gathering_app/views.py
+++ metric_gathering_app/views.py
@@ -17,9 +17,10 @@ jwt_secret_key = os.getenv('JWT_SECRET_KEY')
 
 # ------------------------------ Add Erg Metric Data ------------------------------
 
+
 @api_view(['POST'])
 def add_erg_metric(request):
-    serializer = ErgMetricsSerializer(data = request.data)
+    serializer = ErgMetricsSerializer(data=request.data)
 
     user_id = None
     error_message = ''
@@ -42,50 +43,53 @@ def add_erg_metric(request):
         if not strokes_per_minute:
             error_message = 'No s/m provided'
             culprit = 'strokesPerMinute'
-        
+
         if not time:
             error_message = 'No time provided'
             culprit = 'time'
-        
+
         if error_message == '' and culprit == '':
             # get user
-            user = get_object_or_404(User, user_id = user_id)
+            user = get_object_or_404(User, user_id=user_id)
 
             # parse string into hours, minutes, seconds
             total_seconds = get_time_in_seconds(time)
 
             # check if faster than pb
-            updated_distance = f"pb_{distance.replace('m', '')}" 
+            updated_distance = f"pb_{distance.replace('m', '')}"
 
-            personal_best_object = UserPersonalBests.objects.filter(user_id = user.user_id).first()
+            personal_best_object = UserPersonalBests.objects.filter(
+                user_id=user.user_id).first()
 
             personal_best = PersonalBestsSerializer(personal_best_object)
 
-            pb_in_seconds = get_time_in_seconds(personal_best.data[updated_distance])
-            
+            pb_in_seconds = get_time_in_seconds(
+                personal_best.data[updated_distance])
+
             # add to database
-            formatted_values = add_erg_data(distance, total_seconds, time, user, strokes_per_minute, pb_in_seconds)
+            formatted_values = add_erg_data(
+                distance, total_seconds, time, user, strokes_per_minute, pb_in_seconds)
 
             if pb_in_seconds > total_seconds or pb_in_seconds == 0:
-                setattr(personal_best_object, updated_distance, formatted_values['time'])
+                setattr(personal_best_object, updated_distance,
+                        formatted_values['time'])
                 serializer = PersonalBestsSerializer(personal_best_object)
                 personal_best_object.save()
 
     else:
         print(serializer.errors)
-            
+
     return JsonResponse({
         'errorMessage': error_message,
         'culprit': culprit
     })
 
 
-
 # ------------------------------ Add S&C Metric Data ------------------------------
 
 @api_view(['POST'])
 def add_s_and_c_metric(request):
-    serializer = SAndCMetricsSerializer(data = request.data)
+    serializer = SAndCMetricsSerializer(data=request.data)
 
     user_id = None
     error_message = ''
@@ -97,7 +101,7 @@ def add_s_and_c_metric(request):
 
             error_message = f'Missing {key}'
             culprit = key
-            
+
             return JsonResponse({
                 'errorMessage': error_message,
                 'culprit': culprit
@@ -117,11 +121,11 @@ def add_s_and_c_metric(request):
         # fallback -> shouldn't be necessary
         if len(exercise_list) <= 0:
             exercise_list = ['Bench Press', 'Bicep Curl', 'Deadlift', 'Squat']
-        
+
         if exercise not in exercise_list:
             error_message = 'Invalid exercise'
             culprit = exercise
-        
+
         if not weight:
             error_message = 'No weight provided'
             culprit = 'weight'
@@ -132,11 +136,11 @@ def add_s_and_c_metric(request):
 
         if error_message == '' and culprit == '':
             # get user
-            user = get_object_or_404(User, user_id = user_id)
+            user = get_object_or_404(User, user_id=user_id)
 
             # create new entry in database
-            UserSAndCMetrics.objects.create(user = user, exercise = exercise.lower(), weight = weight, reps = reps)
-
+            UserSAndCMetrics.objects.create(
+                user=user, exercise=exercise.lower(), weight=weight, reps=reps)
 
     else:
         print(serializer.errors)
@@ -148,14 +152,16 @@ def add_s_and_c_metric(request):
 
 # ------------------------------ Helper Functions ------------------------------
 
+
 def get_time_in_seconds(time):
     time_segments = time.split(':')
-            
+
     hours = int(time_segments[0])
     minutes = int(time_segments[1])
     seconds = float(time_segments[2])
 
     return (hours * 3600) + (minutes * 60) + seconds
+
 
 def add_erg_data(distance, total_seconds, time, user, strokes_per_minute, pb_in_seconds):
     # convert distance to number
@@ -164,26 +170,39 @@ def add_erg_data(distance, total_seconds, time, user, strokes_per_minute, pb_in_
     # calculate split
     split = calculate_split(float(new_distance), total_seconds)
 
-    #format
+    # format
     formatted_values = adjust_decimal_places(time, split)
 
     # generate intensity metric for the period that the session occurs
-    
+
     # calculate wattage generated from personal best 500m split
     pb_split = calculate_split(float(new_distance), pb_in_seconds)
+
+    print(pb_split)
 
     pb_watts = calculate_watts(convert_to_seconds(pb_split))
     session_watts = calculate_watts(convert_to_seconds(split))
 
-    intensity = int((session_watts / pb_watts) * 100)
+    intensity = 0
+
+    if pb_watts != 0:
+        intensity = int((session_watts / pb_watts) * 100)
+    else:
+        intensity = 100
 
     # create new entry in database
-    UserErgMetrics.objects.create(user = user, distance = distance, strokes_per_minute = strokes_per_minute, split_500m = formatted_values['split'], time = formatted_values['time'], time_in_seconds = total_seconds, intensity_percentage = intensity)
+    UserErgMetrics.objects.create(user=user, distance=distance, strokes_per_minute=strokes_per_minute,
+                                  split_500m=formatted_values['split'], time=formatted_values['time'], time_in_seconds=total_seconds, intensity_percentage=intensity)
 
     return formatted_values
 
+
 def calculate_watts(split):
-    return 2.8 / (split ** 3)
+    if split != 0:
+        return 2.8 / (split ** 3)
+    else:
+        return 0
+
 
 def convert_to_seconds(time):
     time_segments = time.split(':')
