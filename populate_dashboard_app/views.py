2336c23c52574cb5ced723480a28476540ce64eb
Ben-Christie <ben.christie11@gmail.com>
2023-03-29 11:38:36 +0100

    potential bug fix for time None error when passing values from pb form

--- populate_dashboard_app/views.py
+++ populate_dashboard_app/views.py
@@ -316,15 +316,22 @@ def calculate_user_ratings(all_pbs, my_pbs, time_categories):
 
         for i, pb_times in enumerate(all_personal_bests):
             if time_category in pb_times:
-                time = get_time_in_seconds(pb_times[time_category])
-                my_time = get_time_in_seconds(my_pbs[time_category])
-                new_time_list.append(time)
+                pb_time = pb_times[time_category]
+                user_time = my_pbs[time_category]
+                
+                print(pb_time, user_time)
+                
+                if pb_time != None and user_time != None:
+                    time = get_time_in_seconds(pb_time)
+                    my_time = get_time_in_seconds(user_time)
+                    new_time_list.append(time)
 
         # calculate my_index
         if my_time != None and my_time != '00:00:00':
             for i, time in enumerate(new_time_list):
                 if my_data_index == None and time == my_time:
                     my_data_index = i
+                    print(my_data_index)
 
         # calculate mean and standard deviation
         mean = np.mean(new_time_list)
